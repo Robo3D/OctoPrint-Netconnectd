5189d4e0d08d88b3fbea6f1b967501b3f6bf8100
Victor E <victorevector@gmail.com>
2017-08-14 14:02:23 -0700

    tabs to spaces

--- octoprint_netconnectd/__init__.py
+++ octoprint_netconnectd/__init__.py
@@ -19,257 +19,265 @@ class NetconnectdSettingsPlugin(octoprint.plugin.SettingsPlugin,
                                 octoprint.plugin.SimpleApiPlugin,
                                 octoprint.plugin.AssetPlugin):
 
-	def __init__(self):
-		self.address = None
+    def __init__(self):
+        self.address = None
 
-	def initialize(self):
-		self.address = self._settings.get(["socket"])
+    def initialize(self):
+        self.address = self._settings.get(["socket"])
 
-	@property
-	def hostname(self):
-		hostname = self._settings.get(["hostname"])
-		if hostname:
-			return hostname
-		else:
-			import socket
-			return socket.gethostname() + ".local"
+    @property
+    def hostname(self):
+        hostname = self._settings.get(["hostname"])
+        if hostname:
+            return hostname
+        else:
+            import socket
+            return socket.gethostname() + ".local"
 
-	# ~~ SettingsPlugin
+    # ~~ SettingsPlugin
 
-	def on_settings_save(self, data):
-		octoprint.plugin.SettingsPlugin.on_settings_save(self, data)
-		self.address = self._settings.get(["socket"])
+    def on_settings_save(self, data):
+        octoprint.plugin.SettingsPlugin.on_settings_save(self, data)
+        self.address = self._settings.get(["socket"])
 
-	def get_settings_defaults(self):
-		return dict(
-			socket="/var/run/netconnectd.sock",
-			hostname=None,
-			timeout=10
-		)
+    def get_settings_defaults(self):
+        return dict(
+            socket="/var/run/netconnectd.sock",
+            hostname=None,
+            timeout=10
+        )
 
-	# ~~ TemplatePlugin API
+    # ~~ TemplatePlugin API
 
-	def get_template_configs(self):
-		return [
-			dict(type="settings", name="Network connection")
-		]
+    def get_template_configs(self):
+        return [
+            dict(type="settings", name="Network connection")
+        ]
 
-	# ~~ SimpleApiPlugin API
+    # ~~ SimpleApiPlugin API
 
-	def get_api_commands(self):
-		return dict(
-			start_ap=[],
-			stop_ap=[],
-			refresh_wifi=[],
-			configure_wifi=[],
-			forget_wifi=[],
-			reset=[]
-		)
+    def get_api_commands(self):
+        return dict(
+            start_ap=[],
+            stop_ap=[],
+            refresh_wifi=[],
+            configure_wifi=[],
+            forget_wifi=[],
+            reset=[]
+        )
 
-	def is_api_adminonly(self):
-		return True
+    def is_api_adminonly(self):
+        return True
 
-	def on_api_get(self, request):
-		try:
-			status = self._get_status()
-			if status["wifi"]["present"]:
-				wifis = self._get_wifi_list()
-			else:
-				wifis = []
-		except Exception as e:
-			return jsonify(dict(error=e.message))
+    def on_api_get(self, request):
+        try:
+            status = self._get_status()
+            if status["wifi"]["present"]:
+                wifis = self._get_wifi_list()
+            else:
+                wifis = []
+        except Exception as e:
+            return jsonify(dict(error=e.message))
 
-		return jsonify(dict(
-			wifis=wifis,
-			status=status,
-			hostname=self.hostname
-		))
+        return jsonify(dict(
+            wifis=wifis,
+            status=status,
+            hostname=self.hostname
+        ))
 
-	def on_api_command(self, command, data):
-		if command == "refresh_wifi":
-			return jsonify(self._get_wifi_list(force=True))
+    def on_api_command(self, command, data):
+        if command == "refresh_wifi":
+            return jsonify(self._get_wifi_list(force=True))
 
-		# any commands processed after this check require admin permissions
-		if not admin_permission.can():
-			return make_response("Insufficient rights", 403)
+        # any commands processed after this check require admin permissions
+        if not admin_permission.can():
+            return make_response("Insufficient rights", 403)
 
-		if command == "configure_wifi":
-			if data["psk"]:
-				self._logger.info("Configuring wifi {ssid} and psk...".format(**data))
-			else:
+        if command == "configure_wifi":
+            if data["psk"]:
+                self._logger.info("Configuring wifi {ssid} and psk...".format(**data))
+            else:
                 data['psk'] = ''
-				self._logger.info("Configuring wifi {ssid}...".format(**data))
+                self._logger.info("Configuring wifi {ssid}...".format(**data))
 
-			self._configure_and_select_wifi(data["ssid"], data["psk"], force=data["force"] if "force" in data else False)
+            self._configure_and_select_wifi(data["ssid"], data["psk"], force=data[
+                                            "force"] if "force" in data else False)
 
-		elif command == "forget_wifi":
-			self._forget_wifi()
+        elif command == "forget_wifi":
+            self._forget_wifi()
 
-		elif command == "reset":
-			self._reset()
+        elif command == "reset":
+            self._reset()
 
-		elif command == "start_ap":
-			self._start_ap()
+        elif command == "start_ap":
+            self._start_ap()
 
-		elif command == "stop_ap":
-			self._stop_ap()
+        elif command == "stop_ap":
+            self._stop_ap()
 
-	# ~~ AssetPlugin API
+    # ~~ AssetPlugin API
 
-	def get_assets(self):
-		return dict(
-			js=["js/netconnectd.js"],
-			css=["css/netconnectd.css"],
-			less=["less/netconnectd.less"]
-		)
+    def get_assets(self):
+        return dict(
+            js=["js/netconnectd.js"],
+            css=["css/netconnectd.css"],
+            less=["less/netconnectd.less"]
+        )
 
-	# ~~ Private helpers
+    # ~~ Private helpers
 
-	def _get_wifi_list(self, force=False):
-		payload = dict()
-		if force:
-			self._logger.info("Forcing wifi refresh...")
-			payload["force"] = True
+    def _get_wifi_list(self, force=False):
+        payload = dict()
+        if force:
+            self._logger.info("Forcing wifi refresh...")
+            payload["force"] = True
 
-		flag, content = self._send_message("list_wifi", payload)
-		if not flag:
-			raise RuntimeError("Error while listing wifi: " + content)
+        flag, content = self._send_message("list_wifi", payload)
+        if not flag:
+            raise RuntimeError("Error while listing wifi: " + content)
 
-		result = []
-		for wifi in content:
-			result.append(dict(ssid=wifi["ssid"], address=wifi["address"], quality=wifi["signal"], encrypted=wifi["encrypted"]))
-		return result
+        result = []
+        for wifi in content:
+            result.append(dict(ssid=wifi["ssid"], address=wifi["address"], quality=wifi[
+                          "signal"], encrypted=wifi["encrypted"]))
+        return result
 
-	def _get_status(self):
-		payload = dict()
+    def _get_status(self):
+        payload = dict()
 
-		flag, content = self._send_message("status", payload)
-		if not flag:
-			raise RuntimeError("Error while querying status: " + content)
+        flag, content = self._send_message("status", payload)
+        if not flag:
+            raise RuntimeError("Error while querying status: " + content)
 
-		return content
+        return content
 
-	def _configure_and_select_wifi(self, ssid, psk, force=False):
-		payload = dict(
-			ssid=ssid,
-			psk=psk,
-			force=force
-		)
+    def _configure_and_select_wifi(self, ssid, psk, force=False):
+        payload = dict(
+            ssid=ssid,
+            psk=psk,
+            force=force
+        )
 
-		flag, content = self._send_message("config_wifi", payload)
-		if not flag:
-			raise RuntimeError("Error while configuring wifi: " + content)
+        flag, content = self._send_message("config_wifi", payload)
+        if not flag:
+            raise RuntimeError("Error while configuring wifi: " + content)
 
-		flag, content = self._send_message("start_wifi", dict())
-		if not flag:
-			raise RuntimeError("Error while selecting wifi: " + content)
+        flag, content = self._send_message("start_wifi", dict())
+        if not flag:
+            raise RuntimeError("Error while selecting wifi: " + content)
 
-	def _forget_wifi(self):
-		payload = dict()
-		flag, content = self._send_message("forget_wifi", payload)
-		if not flag:
-			raise RuntimeError("Error while forgetting wifi: " + content)
+    def _forget_wifi(self):
+        payload = dict()
+        flag, content = self._send_message("forget_wifi", payload)
+        if not flag:
+            raise RuntimeError("Error while forgetting wifi: " + content)
 
-	def _reset(self):
-		payload = dict()
-		flag, content = self._send_message("reset", payload)
-		if not flag:
-			raise RuntimeError("Error while factory resetting netconnectd: " + content)
+    def _reset(self):
+        payload = dict()
+        flag, content = self._send_message("reset", payload)
+        if not flag:
+            raise RuntimeError(
+                "Error while factory resetting netconnectd: " + content)
 
-	def _start_ap(self):
-		payload = dict()
-		flag, content = self._send_message("start_ap", payload)
-		if not flag:
-			raise RuntimeError("Error while starting ap: " + content)
+    def _start_ap(self):
+        payload = dict()
+        flag, content = self._send_message("start_ap", payload)
+        if not flag:
+            raise RuntimeError("Error while starting ap: " + content)
 
-	def _stop_ap(self):
-		payload = dict()
-		flag, content = self._send_message("stop_ap", payload)
-		if not flag:
-			raise RuntimeError("Error while stopping ap: " + content)
+    def _stop_ap(self):
+        payload = dict()
+        flag, content = self._send_message("stop_ap", payload)
+        if not flag:
+            raise RuntimeError("Error while stopping ap: " + content)
 
-	def _send_message(self, message, data):
-		obj = dict()
-		obj[message] = data
+    def _send_message(self, message, data):
+        obj = dict()
+        obj[message] = data
 
-		import json
-		js = json.dumps(obj, encoding="utf8", separators=(",", ":"))
+        import json
+        js = json.dumps(obj, encoding="utf8", separators=(",", ":"))
 
-		import socket
-		sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-		sock.settimeout(self._settings.get_int(["timeout"]))
-		try:
-			sock.connect(self.address)
-			sock.sendall(js + '\x00')
+        import socket
+        sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+        sock.settimeout(self._settings.get_int(["timeout"]))
+        try:
+            sock.connect(self.address)
+            sock.sendall(js + '\x00')
 
-			buffer = []
-			while True:
-				chunk = sock.recv(16)
-				if chunk:
-					buffer.append(chunk)
-					if chunk.endswith('\x00'):
-						break
+            buffer = []
+            while True:
+                chunk = sock.recv(16)
+                if chunk:
+                    buffer.append(chunk)
+                    if chunk.endswith('\x00'):
+                        break
 
-			data = ''.join(buffer).strip()[:-1]
+            data = ''.join(buffer).strip()[:-1]
 
-			response = json.loads(data.strip())
-			if "result" in response:
-				return True, response["result"]
+            response = json.loads(data.strip())
+            if "result" in response:
+                return True, response["result"]
 
-			elif "error" in response:
-				# something went wrong
-				self._logger.warn("Request to netconnectd went wrong: " + response["error"])
-				return False, response["error"]
+            elif "error" in response:
+                # something went wrong
+                self._logger.warn(
+                    "Request to netconnectd went wrong: " + response["error"])
+                return False, response["error"]
 
-			else:
-				output = "Unknown response from netconnectd: {response!r}".format(response=response)
-				self._logger.warn(output)
-				return False, output
+            else:
+                output = "Unknown response from netconnectd: {response!r}".format(
+                    response=response)
+                self._logger.warn(output)
+                return False, output
 
-		except Exception as e:
-			output = "Error while talking to netconnectd: {}".format(e.message)
-			self._logger.warn(output)
-			return False, output
+        except Exception as e:
+            output = "Error while talking to netconnectd: {}".format(e.message)
+            self._logger.warn(output)
+            return False, output
 
-		finally:
-			sock.close()
+        finally:
+            sock.close()
 
-	# ~~ Softwareupdate hook
-	def get_update_information(self):
-        	return dict(
-            		netconnectd=dict(
-                		displayName="net connected",
-                		displayVersion=self._plugin_version,
+    # ~~ Softwareupdate hook
+    def get_update_information(self):
+        return dict(
+            netconnectd=dict(
+                displayName="net connected",
+                displayVersion=self._plugin_version,
 
-                		# version check: github repository
-                		type="github_release",
-                		user="Robo3D",
-                		repo="OctoPrint-Netconnectd",
-                		current=self._plugin_version,
+                # version check: github repository
+                type="github_release",
+                user="Robo3D",
+                repo="OctoPrint-Netconnectd",
+                current=self._plugin_version,
 
-                		# update method: pip w/ dependency links
-                		pip="https://github.com/Robo3D/OctoPrint-Netconnectd/archive/{target_version}.zip"
+                # update method: pip w/ dependency links
+                pip="https://github.com/Robo3D/OctoPrint-Netconnectd/archive/{target_version}.zip"
             )
         )
 __plugin_name__ = "Netconnectd Client"
 
-def __plugin_check__():
-	import sys
-	if sys.platform == 'linux2':
-		return True
 
-	logging.getLogger("octoprint.plugins." + __name__).warn("The netconnectd plugin only supports Linux")
-	return False
+def __plugin_check__():
+    import sys
+    if sys.platform == 'linux2':
+        return True
+
+    logging.getLogger("octoprint.plugins." +
+                      __name__).warn("The netconnectd plugin only supports Linux")
+    return False
+
 
 def __plugin_load__():
-	# since we depend on a Linux environment, we instantiate the plugin implementation here since this will only be
-	# called if the OS check above was successful
-	global __plugin_implementation__
-	__plugin_implementation__ = NetconnectdSettingsPlugin()
+    # since we depend on a Linux environment, we instantiate the plugin implementation here since this will only be
+    # called if the OS check above was successful
+    global __plugin_implementation__
+    __plugin_implementation__ = NetconnectdSettingsPlugin()
 
-	global __plugin_hooks__
-	__plugin_hooks__ = {
-        	"octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
-	}
+    global __plugin_hooks__
+    __plugin_hooks__ = {
+        "octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
+    }
 
-	return True
+    return True
